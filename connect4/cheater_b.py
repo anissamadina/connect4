259e100696b058c67220334bd699c3cdf5fbe4e4
anissa <rahlianissa@gmail.com>
2022-10-16 23:59:20 +0200

    win vertical avec cheater

--- connect4/cheater_b.py
+++ connect4/cheater_b.py
@@ -12,10 +12,13 @@ class CheaterB(Player):
                 var = grid.grid[cell][cell2]
                 if var == Cell.A:
                     i += 1
+                else:
+                    i = 0
                     for j in range(4):
-                        print(Grid)
-                        grid.place(j, Cell.B)
-                        print(Grid)
+                        while i < 3:
+                            grid.place(j, Cell.B)
+                            print(Grid)
+                            i += 1
                         return j
 # quand on utilise la commande ci dessous python -m connect4 --player-a CheaterB dans le terminale ça nous revois comme quoi B wins mais quand on essaye de faire le unitest cela ne fonctionne pas
 
